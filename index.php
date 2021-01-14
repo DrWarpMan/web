a05dfab1c2236c63e60279fbaa5c77e699c0fa0c
DrWarpMan <DrWarpMan@gmail.com>
2021-01-14 10:31:18 +0100

    removed old hover css

--- index.php
+++ index.php
@@ -20,9 +20,6 @@ $data = getData();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-    <!-- Hover CSS -->
-    <link rel="stylesheet" href="./css/hover.css" media="all">
-
     <!-- My CSS - Always last -->
     <link rel="stylesheet" href="./css/style.css">
 
